322ad31b0249e7949715528281f55d1e5c460a9d
Kyosuke-Fukui <fs72os.kyosuke@gmail.com>
2024-09-24 19:14:03 +0900

    Update next.config.js and GitHub Actions settings

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    esmExternals: 'loose'
-  },
-  webpack: (config) => {
-    config.externals = [...config.externals, { canvas: "canvas" }]; // required to make pdfjs work
-    return config;
+  output: "export",
+  basePath: "/portfoliio3",
+  images: {
+    unoptimized: true,
   },
 };
 
